d2734d4bca77b584697be9b358caabffad3f2263
SunjidaAkter <sanjidabintesayid7@gmail.com>
2022-08-13 16:57:44 +0600

    index.js updated

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './app/store';
+import './Components/ProductDetail/Buy.scss'
 import { fetchProducts } from './features/Products/productsSlice';
 
 
